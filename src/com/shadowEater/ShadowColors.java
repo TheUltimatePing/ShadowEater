6d1544ad917e397fdb265b2a98f36c633a60cd80
TheUltimatePing <liamlego@gmail.com>
2025-12-08 23:07:06 +0100

    the color screen is opening

--- src/com/shadowEater/ShadowColors.java
+++ src/com/shadowEater/ShadowColors.java
@@ -2,6 +2,8 @@ package com.shadowEater;
 
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 // used to select the colors
 public class ShadowColors extends JFrame {
@@ -9,17 +11,38 @@ public class ShadowColors extends JFrame {
         // same image as the main frame
         ImageIcon appIcon = new ImageIcon("src/image/logo.jpg");
         String titleName = "Shadow Eater color selector";
+        Border paidBorder;
+
+        paidBorder = BorderFactory.createLineBorder(Color.getColor("D3AF37")); // set the color to golden
+
+        // the panel of the screen
+        JPanel colorPanel = new JPanel();
+        colorPanel.setLayout(new GridLayout(64, 2)); // 64 to be sure
 
         WPlaceColor[] colors = WPlaceColor.values();
-        // creation of the button colors
-        for (WPlaceColor color : colors) {
 
+        // creation of the button colors
+        //we will have 2 "groups" for paid and free colors
+        for (WPlaceColor color : colors) {
+            JButton btn = new JButton(color.getName());
+            btn.setBackground(new Color(color.getColor()));
+            btn.setName(color.getName());
+
+            if (color.getIsPaid()) {
+                btn.setBorder(paidBorder);
+            }
+
+            // we add each button to the pannel
+            colorPanel.add(btn);
         }
+
+        colorPanel.revalidate();
+        colorPanel.repaint();
 
         // what do the cross does
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        // automatically set the page to full sized
-        setSize(50, 50);
+        // automatically set the page to full size
+        setSize(500, 500);
         // set the icon to the app icon
         setIconImage(appIcon.getImage());
         // set the name
